329d98edd09812635efd830afb2ad6e7417c7677
Timmie Bark <timmie.bark@hotmail.com>
2019-01-23 08:39:37 +0100

    Added Picks.Core, Infrastructure, entityFramework and AutoMapper dependencies

--- Picks2.web/Controllers/HomeController.cs
+++ Picks2.web/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Picks2.web.Models;
 
 namespace Picks2.web.Controllers
 {
@@ -32,12 +31,6 @@ namespace Picks2.web.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
