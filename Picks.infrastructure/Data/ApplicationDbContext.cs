8393c33973d76399707118943b5bb64550fc76c4
Timmie Bark <timmie.bark@hotmail.com>
2019-01-23 09:15:28 +0100

    Added Database context class

---
+++ Picks.infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Picks.core.Entities;
+
+namespace Picks.infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
