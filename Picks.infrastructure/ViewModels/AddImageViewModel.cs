15f4c2bad4967c83643c78b353f28bb7e85ea465
Timmie Bark <timmie.bark@hotmail.com>
2019-01-24 08:05:41 +0100

    Form for uploading images

---
+++ Picks.infrastructure/ViewModels/AddImageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Picks.core.Entities;
+using Picks.infrastructure.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picks.infrastructure.ViewModels
+{
+    public class AddImageViewModel : BaseViewModel
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+    }
+}
